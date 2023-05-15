7ada9401e65852575a7fc973bd00295bd5051e30
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-05-15 19:48:23 -0300

    add validation in create category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,12 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body
 
+    const nameAlreadyExist = categoriesRepository.findByName(name)
+
+    if(nameAlreadyExist) {
+        return res.status(400).json({error: "Category Already Exists"})
+    }
+
     categoriesRepository.create({ name, description })
 
     return res.status(201).send()
