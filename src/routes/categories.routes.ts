c7e39f14cfff91fcef157fd6eabcdcacf2ee9521
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-05-29 19:42:47 -0300

    feat: create controller and useCase for list categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
@@ -10,9 +11,7 @@ categoriesRoutes.post("/", (req, res) => {
 })
 
 categoriesRoutes.get("/", (req, res) => {
-    const all = categoriesRepository.list()
-
-    return res.json(all)
+    return listCategoriesController.handle(req, res)
 })
 
 export { categoriesRoutes }
