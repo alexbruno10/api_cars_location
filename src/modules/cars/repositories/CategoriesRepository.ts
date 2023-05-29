f527ce8706049195bdacef7ccecdd1894f5b4361
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-05-29 19:55:19 -0300

    feat: add singleton

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -6,8 +6,18 @@ class CategoriesRepository implements ICategoryRepository {
 
     private categories: Category[]
 
-    constructor() {
+    //singleton para instanciar uma classe
+    private static INSTANCE: CategoriesRepository
+
+    private constructor() {
         this.categories = []
+    }
+
+    public static getInstance(): CategoriesRepository {
+        if(!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository()
+        }
+        return CategoriesRepository.INSTANCE
     }
 
     create( {name, description} : ICreateCategoryDTO ) : void {
