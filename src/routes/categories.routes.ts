392b0eabcf8e0046bc18823712184223f7b77b1d
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-05-30 19:47:51 -0300

    feat: create route of import

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,9 +2,16 @@ import { Router } from 'express'
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
+import multer from 'multer'
 
 const categoriesRoutes = Router()
 // const categoriesRepository = CategoriesRepository.getInstance()
+
+//middleware responsavel por pegar o arquivo
+const upload = multer({
+    dest: "./tmp",
+})
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res)
@@ -14,5 +21,9 @@ categoriesRoutes.get("/", (req, res) => {
     return listCategoriesController.handle(req, res)
 })
 
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+    return importCategoryController.handle(req, res)
+})
+
 export { categoriesRoutes }
- 
+  
