88fb2f6fd6a31289844003623e5e6374aa1629a3
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-05-30 21:46:25 -0300

    feat: install and configurate swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express, { Router } from 'express'
-import { categoriesRoutes } from './routes/categories.routes'
-import { specificationsRoutes } from './routes/specifications.routes'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerFile from './swagger.json'
+
 import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
