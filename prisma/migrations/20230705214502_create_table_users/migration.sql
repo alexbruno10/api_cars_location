50b43daec3fc1ba52ad7e503e382c811f6fdff23
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-07-05 18:50:36 -0300

    feat: create table users

---
+++ prisma/migrations/20230705214502_create_table_users/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "driver_licence" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
