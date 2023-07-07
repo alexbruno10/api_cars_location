211d7879b1210df4b08be8963ecd16ae41dd7037
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-07-06 21:07:42 -0300

    feat: create user entity

---
+++ src/modules/account/entities/User.ts
@@ -0,0 +1,20 @@
+import {v4 as uuidv4} from 'uuid'
+
+class User {
+    id?: string;
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    driver_licence: string;
+    isAdmin: boolean;
+    created_at: Date
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuidv4()
+        }
+    }
+}
+
+export { User }
