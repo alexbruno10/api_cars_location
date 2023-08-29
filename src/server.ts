712a304034fe6e80513af476024097e05a72d0d5
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-08-29 19:51:26 -0300

    feat: create verification instanceof

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, { Router } from 'express'
+import express, { NextFunction, Request, Response, Router } from 'express'
+import "express-async-errors"
 import swaggerUi from 'swagger-ui-express'
 import 'reflect-metadata'
 import './shared/container'
@@ -6,6 +7,7 @@ import './shared/container'
 import swaggerFile from './swagger.json'
 
 import { router } from './routes'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
@@ -14,6 +16,19 @@ app.use(express.json())
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    })
+})
 
 app.listen(3333, () => console.log('Server is running!'))
 
